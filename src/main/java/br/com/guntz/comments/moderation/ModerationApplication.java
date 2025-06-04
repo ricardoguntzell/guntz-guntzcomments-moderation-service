8dd025704b91107f047a14c03ec16e6bbb8a6181
guntz <rricardoguntzell@gmail.com>
2025-06-04 17:15:04 -0300

    feat: inclusao de projeto spring

---
+++ src/main/java/br/com/guntz/comments/moderation/ModerationApplication.java
@@ -0,0 +1,13 @@
+package br.com.guntz.comments.moderation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModerationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/guntz/comments/moderation/ModerationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guntz.comments.moderation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
